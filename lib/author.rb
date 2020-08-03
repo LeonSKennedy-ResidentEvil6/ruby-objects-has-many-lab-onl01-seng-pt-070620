29c576d3b168ce664469270bdd79e55adb001f48
Ebo Lee <yiboli.coding@gmail.com>
2020-08-03 21:13:11 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -17,7 +17,7 @@ class Author
   end
 
   # take in arg & post title, create new post, associate it with author
-  def add_post_by_title(post, title)
+  def add_post_by_title(post)
     post = Post.new(post, title)
     add_post(post)
   end
